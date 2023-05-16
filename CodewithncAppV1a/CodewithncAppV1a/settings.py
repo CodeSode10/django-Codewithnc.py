a404753395dec3411d82157d4c6331d72a682b0d
Code With Hindi <123615985+CodeWithHindi@users.noreply.github.com>
2023-05-17 01:18:48 +0530

    Update settings.py

--- CodewithncAppV1a/CodewithncAppV1a/settings.py
+++ CodewithncAppV1a/CodewithncAppV1a/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = ['192.168.43.92', '127.0.0.1']
+ALLOWED_HOSTS = ['13.50.109.130']
 
 
 # Application definition
